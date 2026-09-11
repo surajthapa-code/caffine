8c0b95be76fb74627fb3fc910ba18d59e487b874
surajthapa-code <surajthapa7787@gmail.com>
2026-09-11 18:24:39 +0530

    refactor: update booking and user models for clarity and consistency

--- modal/booking.modal.ts
+++ modal/booking.modal.ts
@@ -1,20 +1,19 @@
 import mongoose, { Schema } from "mongoose";
 
 export type BookingStatus =
-	| "pending"
-	| "approved"
-	| "rejected"
-	| "cancelled"
-	| "completed";
+	| "pending" // by default 
+	| "approved" // by admin
+	| "rejected" // by admin
+	| "cancelled" // by user under 15 minutes
+	| "completed"; // after the booking is done and time is over, will be marked as completed by the system
 
 export interface BookingDocument {
-	_id: number;
-	venue_id: number;
+	venue_id: mongoose.Types.ObjectId;
 	user_id: mongoose.Types.ObjectId;
 	starting_time: string;
 	ending_time: string;
 	date: Date;
-	numberofstudent: number;
+	numberofStudents: number;
 	status: BookingStatus;
 	reason: string;
 }
@@ -22,9 +21,9 @@ export interface BookingDocument {
 const BookingSchema = new Schema<BookingDocument>(
 	{
 		venue_id: { 
-			type: Number,
-			required: true,
+			type: Schema.Types.ObjectId,
 			ref: "Venue",
+			required: true,
 		},
 		user_id: {
 			type: Schema.Types.ObjectId,
@@ -34,6 +33,9 @@ const BookingSchema = new Schema<BookingDocument>(
 		starting_time: { type: String, required: true,  enum: [ "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00"]},
 		ending_time: { type: String, required: true , enum: [ "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00"]},
 		date: { type: Date, required: true },
+		numberofStudents: { type: Number, min: 1 },
+		 // ~~~ no of students that will be attending the event, 
+		// ~~~ if numberOfStudents > venue's capacity,reject the booking 
 		status: {
 			type: String,
 			enum: ["pending", "approved", "rejected", "cancelled", "completed"], // approved by admin, rejected by admin, cancelled by user, completed after the booking is done
--- modal/venue.modal.ts
+++ modal/venue.modal.ts
@@ -1,9 +1,8 @@
 import mongoose, { Schema } from "mongoose";
 
 export interface VenueDocument {
-	_id: number;
-	capablity: number;
-	image: string;
+	capacity: number;
+	images: [string];
 	name: string;
 	building: string;
 	isActive: boolean;
@@ -13,8 +12,8 @@ export interface VenueDocument {
 
 const VenueSchema = new Schema<VenueDocument>(
 	{
-		capablity: { type: Number, required: true, min: 1 },  // no of students that can be accomodated in the venue
-		image : { type: String, trim: true }, // image of the venue, imagekit will be used to upload the image and get the url, this url will be stored in the database
+		capacity: { type: Number, required: true, min: 1 },  // no of students that can be accomodated in the venue
+		images: [{ type: String, trim: true }], // images of the venue, imagekit will be used to upload the image and get the url, this url will be stored in the database
 		name: { type: String, required: true, trim: true }, // name of the venue/room
 		building: { type: String, required: true, trim: true },  // builidng in which the venue is located
 		isActive: { type: Boolean, default: true },  // is the venue active or not, if not active then it cannot be booked
